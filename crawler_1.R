9993d70d667d9d0827927dc343ff966c47c0df80
Calvin-sen <tianht.18@sem.tsinghua.edu.cn>
2020-11-15 19:43:13 +0800

    download the table

--- crawler_1.R
+++ crawler_1.R
@@ -2,3 +2,10 @@ install.packages("rvest")
 library(rvest)
 url_NBS <- "http://www.mca.gov.cn/article/sj/xzqh/1980/2019/202002281436.html"
 web_NBS <- read_html(url_NBS)
+
+#下载表格#
+table <- html_table(web_NBS)
+
+#数据处理#
+table_1 <- table[[1]][-c(1,2,3), -c(1,4,5,6,7,8,9)]
+head(table_1)
