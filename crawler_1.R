e2adaa4e8ddb922c7d5a5c6552a31ac5b0308a36
Calvin-sen <tianht.18@sem.tsinghua.edu.cn>
2020-11-18 10:51:53 +0800

    Update crawler_1.R
    improve the strcuture

--- crawler_1.R
+++ crawler_1.R
@@ -20,17 +20,21 @@ table_2 <- data.frame(table_1, prov, prov_coun, city)
 
 #河北省1986年数据#
 hebei <- subset(table_2, table_2[,3]==13)
-hebei_1986 <- subset(hebei, hebei[,5]=="00"|hebei[,4]=="90")
+hebei_1986_1 <- subset(hebei, hebei[,5]=="00"|hebei[,4]=="90")
+hebei_1986 <- hebei_1986_1[,-c(3,4,5)]
 
 #辽宁省1986年数据#
 liaoning <- subset(table_2, table_2[,3]==21)
-liaoning_1986  <- subset(liaoning, liaoning[,5]=="00"|liaoning[,4]=="90")
+liaoning_1986_1  <- subset(liaoning, liaoning[,5]=="00"|liaoning[,4]=="90")
+liaoning_1986 <- liaoning_1986_1[,-c(3,4,5)]
 
 #吉林省1986年数据#
 jilin <- subset(table_2, table_2[,3]==22)
-jilin_1986 <- subset(jilin, jilin[,5]=="00"|jilin[,4]=="90")
+jilin_1986_1 <- subset(jilin, jilin[,5]=="00"|jilin[,4]=="90")
+jilin_1986 <- jilin_1986_1[,-c(3,4,5)]
 
 #黑龙江省1986年数据#
 heilongjiang <- subset(table_2, table_2[,3]==23)
-heilongjiang_1986 <- subset(heilongjiang, heilongjiang[,5]=="00"|heilongjiang[,4]=="90")
+heilongjiang_1986_1 <- subset(heilongjiang, heilongjiang[,5]=="00"|heilongjiang[,4]=="90")
+heilongjiang_1986 <- heilongjiang_1986_1[,-c(3,4,5)]
 
