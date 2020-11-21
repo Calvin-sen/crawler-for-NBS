eb446caf2009256c0ef2499b10556c24262c58d6
Calvin-sen <tianht.18@sem.tsinghua.edu.cn>
2020-11-22 00:23:27 +0800

    correct the links

--- crawler_total.R
+++ crawler_total.R
@@ -1,6 +1,10 @@
 library(rvest)
 library(tidyverse)
 library(stringr)
+library(devtools)
+library(Rwebdriver)
+library(XML)
+library(RCurl)
 
 #爬取主页面#
 web <- vector("list", 3)
@@ -15,9 +19,18 @@ for(i in 1:2)
 
 #爬取每年的网站链接#
 year_link <- vector("list", 3)
+year_link_m <- vector("list", 3)
 for(i in 1:3)
 {
-  year_link[[i]] <- html_nodes(web[[i]], "div.alist a") %>% html_attr("href")
+  year_link_m[[i]] <- html_nodes(web[[i]], "div.alist a") %>% html_attr("href")
+  i = i+1
+}
+head(year_link_m)
+for(i in 1:3)
+{
+  year_link[[i]] <- paste0("http://www.mca.gov.cn", year_link_m[[i]])
   i = i+1
 }
 head(year_link)
+
+
